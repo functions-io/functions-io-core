ba8ddfa704ccd2a5af89f36c1881edbe13f65c1f
Rogério Dias Moreira <rogeriodegoiania@gmail.com>
2017-03-12 11:04:43 -0300

    Track functions changed

---
+++ examples/reload.js
@@ -0,0 +1,49 @@
+var functionsio = require("../");
+var app = null;
+var config = {};
+
+var reloadFiles = function(){
+    app.loadFunctions(function(err, result){
+        if (err){
+            console.error(err);
+        }
+        else{
+            console.log('reloaded\n');
+            callFunction();
+            //console.log(result);
+        }
+    });
+}
+
+var callFunction = function(){
+    try{
+        app.factory.invoke(null, "subfolder.subfolder3.sum", "v1", {x:2,y:3}, null, function(err, data){
+            if (err){
+                console.error("err in invoke subfolder.subfolder3.sum-v1 -> " + err);
+            }
+            else{
+                console.log("Call subfolder.subfolder3.sum-v1(2, 3) = " + data.value);
+            }
+        });
+    }
+    catch(err){
+        console.log("subfolder.subfolder3.sum-v1 ->" + err.message);
+    }
+}
+
+config.isGenerateStatistics = true;
+config.unitTest = {};
+config.unitTest.load = false;
+config.unitTest.executeOnStart = false;
+
+app = functionsio.createServer(config);
+app.pathFunctions = __dirname + "/../test/functions";
+
+app.start(function(err){
+    if (err){
+        console.error(err);
+    }
+    else{
+        setInterval(reloadFiles, 10000);
+    }
+});
---
+++ test/functions/subfolder/subfolder3/sum.js
@@ -0,0 +1,17 @@
+"use strict";
+
+module.category = "test2";
+module.summary = "sum";
+module.description = "sum x + y";
+
+module.input = {
+    x:{type:"integer", required:true},
+    y:{type:"integer", required:true}
+};
+module.output = {
+    value:{type:"integer"}
+};
+
+module.exports = function(context, message, callBack){
+    callBack(null, {value: message.x + message.y});
+};
