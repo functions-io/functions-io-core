c89e839086214bbb12c52ce3fcd62b084b3c7a72
Rogério Dias Moreira <rogeriodegoiania@gmail.com>
2018-05-03 00:23:36 -0300

    init

--- labs/math.sum.test.js
+++ labs/math.sum.test.js
@@ -1,11 +1,11 @@
 const path = require("path");
 
 const functionsio = require("../");
-const moduleFactory = functionsio.buildModuleFactory({pathModules: path.join(process.cwd(), "labs", "functions")});
+const moduleFactory = functionsio.buildModuleFactory();
 
 var tempo1 = new Date().getTime();
 
-moduleFactory.requireAsync("@my-company/math.sum", "2")
+moduleFactory.requireAsync("@my-company/math.sum", "1")
     .then(function(moduleObj){
         console.log(moduleObj(2,3));
         var tempo2 = new Date().getTime();
