92174c78efc84323dc22ce987fbdabb30ec89a88
Rogério Dias Moreira <rogeriodegoiania@gmail.com>
2018-05-05 20:45:23 -0300

    init

--- labs/moduleFactoryTest3.js
+++ labs/moduleFactoryTest3.js
@@ -1,8 +1,13 @@
 const functionsio = require("../");
 const moduleFactory = functionsio.buildModuleFactory();
 
+var tempo1 = new Date().getTime();
+
 moduleFactory.requireAsync("chalk", "2")
     .then(function(chalk){
+        var tempo2 = new Date().getTime();
+        console.log("time: ", (tempo2 - tempo1));
+        
         console.log(chalk);
         console.log(chalk.blue('Hello world!'));
     }).catch(function(err){
