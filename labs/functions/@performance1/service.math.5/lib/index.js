7103725d84134ebd687804719ee60ba62a49d74f
Rogério Dias Moreira <rogeriodegoiania@gmail.com>
2018-05-07 23:11:02 -0300

    performance

---
+++ labs/functions/@performance1/service.math.5/lib/index.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const mathSum1 = require("@performance1/math.sum.1");
+const mathSum2 = require("@performance1/math.sum.2");
+const mathSum3 = require("@performance1/math.sum.3");
+const mathSum4 = require("@performance1/math.sum.4");
+const mathSum5 = require("@performance1/math.sum.5");
+
+module.exports.sum = function(x,y){
+    mathSum5(2,3);
+    mathSum4(2,3);
+    mathSum3(2,3);
+    mathSum2(2,3);
+    mathSum1(2,3);
+
+    return x + y;
+}
---
+++ labs/functions/@performance1/test/service.math.20.js
@@ -0,0 +1,12 @@
+const functionsio = require("../../../../");
+const moduleFactory = functionsio.buildModuleFactory();
+
+var tempo1 = new Date().getTime();
+moduleFactory.requireAsync("@performance1/service.math.20", "1")
+    .then(function(moduleObj){
+        var tempo2 = new Date().getTime();
+        moduleObj.sum(2,3);
+        console.log("time: ", (tempo2 - tempo1));
+    }).catch(function(err){
+        console.log("erro", err);
+    });
