d1d8b7b7d6d74b05a7145b8e747073c116bbe0e4
Rogério Dias Moreira <rogeriodegoiania@gmail.com>
2018-05-09 20:13:30 -0300

    performance3

---
+++ labs/functions/@performance3/service.math.1/lib/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const mathSum1 = require("@performance1/math.sum.1");
+
+module.exports.sum = function(x,y){
+    mathSum1(2,3);
+    
+    return x + y;
+}
---
+++ labs/functions/@performance3/service.math.20/lib/index.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const mathSum1 = require("@performance1/math.sum.1");
+const mathSum2 = require("@performance1/math.sum.2");
+const mathSum3 = require("@performance1/math.sum.3");
+const mathSum4 = require("@performance1/math.sum.4");
+const mathSum5 = require("@performance1/math.sum.5");
+const mathSum6 = require("@performance1/math.sum.6");
+const mathSum7 = require("@performance1/math.sum.7");
+const mathSum8 = require("@performance1/math.sum.8");
+const mathSum9 = require("@performance1/math.sum.9");
+const mathSum10 = require("@performance1/math.sum.10");
+const mathSum11 = require("@performance1/math.sum.11");
+const mathSum12 = require("@performance1/math.sum.12");
+const mathSum13 = require("@performance1/math.sum.13");
+const mathSum14 = require("@performance1/math.sum.14");
+const mathSum15 = require("@performance1/math.sum.15");
+const mathSum16 = require("@performance1/math.sum.16");
+const mathSum17 = require("@performance1/math.sum.17");
+const mathSum18 = require("@performance1/math.sum.18");
+const mathSum19 = require("@performance1/math.sum.19");
+const mathSum20 = require("@performance1/math.sum.20");
+
+module.exports.sum = function(x,y){
+    mathSum20(2,3);
+    mathSum19(2,3);
+    mathSum18(2,3);
+    mathSum17(2,3);
+    mathSum16(2,3);
+    mathSum15(2,3);
+    return x + y;
+}
