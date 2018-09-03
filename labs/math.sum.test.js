29953a4752594fb4e731dff73470a73eae0f99be
Rogério Dias Moreira <rogeriodegoiania@gmail.com>
2018-09-03 20:29:13 -0300

    init

--- labs/math.sum.test.js
+++ labs/math.sum.test.js
@@ -13,13 +13,3 @@ moduleFactory.requireAsync("@my-company/math.sum", "1")
     }).catch(function(err){
         console.log("erro", err);
     });
-
-setTimeout(function(){
-    console.log("test 2");
-    moduleFactory.invokeAsync("@my-company/math.multiply", "1", [2,10])
-        .then(function(result){
-            console.log(result);
-        }).catch(function(err){
-            console.log("erro", err);
-        });
-}, 1000);
