ce2d1b5ce45c01c53388a1155af1433b02f9ad7e
Rogério Dias Moreira <rogeriodegoiania@gmail.com>
2018-09-07 11:02:32 -0300

    init

---
+++ labs/uuid.test.js
@@ -0,0 +1,13 @@
+const functionsio = require("../");
+const moduleFactory = functionsio.buildModuleFactory();
+
+var tempo1 = new Date().getTime();
+
+moduleFactory.requireAsync("uuid/v4", "*")
+    .then(function(moduleObj){
+        console.log(moduleObj());
+        var tempo2 = new Date().getTime();
+        console.log("time: ", (tempo2 - tempo1));
+    }).catch(function(err){
+        console.log("erro", err);
+    });
