dd8a24f8fea3508cdb192998cb112bad2b3c5d20
Rogério Dias Moreira <rogeriodegoiania@gmail.com>
2018-09-26 20:42:42 -0300

    refactoring

--- labs/requireAsync/service.math.sum.test.js
+++ labs/requireAsync/service.math.sum.test.js
@@ -2,11 +2,9 @@ const functionsio = require("../../lib");
 const moduleFactory = functionsio.buildModuleFactory();
 
 var tempo1 = new Date().getTime();
-
-moduleFactory.requireAsync("@my-company/service.math", "1")
+moduleFactory.requireAsync("@functions-io-labs/service.sum", "1")
     .then(function(moduleObj){
-        console.log(moduleObj.sum(5,5));
-        console.log(moduleObj.multiply(5,5));
+        console.log(moduleObj(5,5));
         var tempo2 = new Date().getTime();
         console.log("time: ", (tempo2 - tempo1));
     }).catch(function(err){
