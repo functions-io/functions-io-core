17b5c1d159f42a772b83e0145e578e37d48d0def
Rogério Dias Moreira <rogeriodegoiania@gmail.com>
2018-09-28 11:11:29 -0300

    refactoring

---
+++ labs/manifestAsync/getManifest.js
@@ -0,0 +1,9 @@
+const functionsio = require("../../lib");
+const invokeFactory = functionsio.buildInvokeFactory();
+
+invokeFactory.geManifestAsync("@functions-io-labs/math.sum.async", "1")
+    .then(function(result){
+        console.log("test1", "result => ", result);
+    }).catch(function(err){
+        console.log("test1", "err", err);
+    });
